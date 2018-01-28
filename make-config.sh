69cd2d0fe68cbc10dcea29ecbedcc48845456279
advena <m.gepert@gmail.com>
2018-01-28 21:23:21 +0100

    base make-config.sh script to apply all config files

---
+++ make-config.sh
@@ -0,0 +1,25 @@
+cd $HOME/.config
+mv Atom Atom.bak
+ln -s ../dotfiles/config/Atom
+mv autostart autostart.bak
+ln -s ../dotfiles/config/autostart 
+mv clipit clipit.bak
+ln -s ../dotfiles/config/clipit
+mv compton.conf compton.conf.bak
+ln -s ../dotfiles/config/compton.conf 
+mv gtk-2.0 gtk-2.0.bak
+ln -s ../dotfiles/config/gtk-2.0
+mv gtk-3.0 gtk-3.0.bak
+ln -s ../dotfiles/config/gtk-3.0
+mv i3 i3.bak
+ln -s ../dotfiles/config/i3
+mv i3status i3status.bak
+ln -s ../dotfiles/config/i3status
+mv polybar polybar.bak
+ln -s ../dotfiles/config/polybar
+mv terminator terminator.bak
+ln -s ../dotfiles/config/terminator
+mv xfce4 xfce4.bak
+ln -s ../dotfiles/config/xfce4 
+
+
